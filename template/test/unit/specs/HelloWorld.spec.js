b815af11be82b93767316c9dbda4b198fad681dc
Bart Ledoux <ledouxb@gmail.com>
2017-12-03 17:04:11 -0600

    use vue test utils

--- template/test/unit/specs/HelloWorld.spec.js
+++ template/test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import HelloWorld from '@/components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import { mount } from 'vue-test-utils'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    const vm = new Constructor().$mount(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    const wrapper = mount(HelloWorld){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    expect(wrapper.find('.hello h1').element.textContent)
     {{#if_eq runner "karma"}}.to.equal('Welcome to Your Vue.js App'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}{{/if_eq}}{{#if_eq runner "jest"}}.toEqual('Welcome to Your Vue.js App'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}{{/if_eq}}
   }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
